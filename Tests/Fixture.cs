700de2c94dc3cf04271375986a68a47a32f65f83
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-09-19 22:46:31 +0700

    Added tests for HackathonOrganizer

--- Tests/Fixture.cs
+++ Tests/Fixture.cs
@@ -1,6 +1,25 @@
+using Contracts;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Tests;
 
 public class Fixture
 {
-    
+    private readonly IServiceScope _scope;
+
+    protected Fixture()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddTransient<IHarmonizationCalculator, HrDirector.HrDirector>();
+        serviceCollection.AddTransient<ITeamBuildingStrategy, HrManager.HrManager>();
+        serviceCollection.AddTransient<IHackathonOrganizer, HackathonOrganizer.HackathonOrganizer>();
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        _scope = serviceProvider.CreateScope();
+    }
+
+    protected T GetService<T>()
+    {
+        return _scope.ServiceProvider.GetRequiredService<T>();
+    }
 }
---
+++ Tests/HackathonOrganizerTests.cs
@@ -0,0 +1,86 @@
+using Contracts;
+using Xunit;
+
+namespace Tests;
+
+public class HackathonOrganizerTests : Fixture
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    [InlineData(10)]
+    [InlineData(50)]
+    [InlineData(100)]
+    public void TestPerfectMatch(int count)
+    {
+        var teamLeads = new List<Employee>();
+        var juniors = new List<Employee>();
+        var teamLeadsWishlists = new List<Wishlist>();
+        var juniorsWishlists = new List<Wishlist>();
+
+        var desiredEmployees = Enumerable.Range(1, count).ToList();
+
+        for (var i = 0; i < count; i++)
+        {
+            var id = i + 1;
+            teamLeads.Add(new Employee(id, $"Team-lead {id}"));
+            juniors.Add(new Employee(id, $"Junior {id}"));
+            teamLeadsWishlists.Add(new Wishlist(id, desiredEmployees.ToArray()));
+            juniorsWishlists.Add(new Wishlist(id, desiredEmployees.ToArray()));
+            ShiftLeft(desiredEmployees, 1);
+        }
+
+        var members = GetService<IHackathonOrganizer>()
+            .Organize(teamLeads, juniors, teamLeadsWishlists, juniorsWishlists);
+
+        Assert.Equal(count, Math.Round(members.Harmonization));
+    }
+
+    [Fact]
+    public void TestSomePreferences()
+    {
+        const int count = 4;
+        var teamLeads = new List<Employee>();
+        var juniors = new List<Employee>();
+
+        for (var i = 0; i < count; i++)
+        {
+            var id = i + 1;
+            teamLeads.Add(new Employee(id, $"Team-lead {id}"));
+            juniors.Add(new Employee(id, $"Junior {id}"));
+        }
+
+        List<Wishlist> teamLeadsWishlists =
+        [
+            new(1, [1, 2, 3, 4]),
+            new(2, [1, 2, 3, 4]),
+            new(3, [1, 2, 3, 4]),
+            new(4, [1, 2, 3, 4])
+        ];
+
+        List<Wishlist> juniorsWishlists =
+        [
+            new(1, [2, 4, 3, 1]),
+            new(2, [2, 4, 1, 3]),
+            new(3, [1, 2, 3, 4]),
+            new(4, [3, 4, 1, 2])
+        ];
+
+        var members = GetService<IHackathonOrganizer>()
+            .Organize(teamLeads, juniors, teamLeadsWishlists, juniorsWishlists);
+
+        Assert.Equal(2.526, Math.Round(members.Harmonization, 3));
+    }
+
+    private static void ShiftLeft<T>(List<T> list, int shift)
+    {
+        var copy = new List<T>(list);
+
+        for (var i = shift; i < list.Count; i++) list[i - shift] = list[i];
+
+        for (var i = list.Count - shift; i < list.Count; i++) list[i] = copy[i + shift - list.Count];
+    }
+}
