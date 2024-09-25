c393294401a7d4fc20b90b67c3eeb65121e9d44d
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-09-25 19:57:02 +0700

    Refactored WishlistsProvider

--- HackathonProblem.Tests/WishlistsProviderTests.cs
+++ HackathonProblem.Tests/WishlistsProviderTests.cs
@@ -6,60 +6,63 @@ namespace HackathonProblem.Tests;
 
 public class EmployeesGenerator : IEnumerable<object[]>
 {
-    private readonly List<object[]> _data =
+    private static readonly List<Employee> TeamLeads1 = [new(1, "Team-lead 1")];
+    private static readonly List<Employee> Juniors1 = [new(1, "Junior 1")];
+    private static readonly List<Employee>[] Members1 = [TeamLeads1, Juniors1];
+    
+    private static readonly List<Employee> TeamLeads2 =
     [
-        new object[]
-        {
-            new List<Employee> { new(1, "Team-lead 1") },
-            new List<Employee> { new(1, "Junior 1") }
-        },
-        new object[]
-        {
-            new List<Employee>
-            {
-                new(1, "Team-lead 1"),
-                new(2, "Team-lead 2")
-            },
-            new List<Employee> { new(13, "Junior 13") }
-        },
-        new object[]
-        {
-            new List<Employee>
-            {
-                new(1, "Team-lead 1"),
-                new(2, "Team-lead 2")
-            },
-            new List<Employee>
-            {
-                new(6, "Junior 6"),
-                new(17, "Junior 17")
-            }
-        },
-        new object[]
-        {
-            new List<Employee>
-            {
-                new(1, "Team-lead 1"),
-                new(2, "Team-lead 2"),
-                new(3, "Team-lead 3"),
-                new(4, "Team-lead 4")
-            },
-            new List<Employee>
-            {
-                new(101, "Junior 101"),
-                new(102, "Junior 102"),
-                new(103, "Junior 103"),
-                new(104, "Junior 104"),
-                new(105, "Junior 105"),
-                new(106, "Junior 106"),
-                new(107, "Junior 107")
-            }
-        }
+        new(1, "Team-lead 1"),
+        new(2, "Team-lead 2")
+    ];
+    private static readonly List<Employee> Juniors2 =
+    [
+        new(13, "Junior 13")
+    ];
+    private static readonly List<Employee>[] Members2 = [TeamLeads2, Juniors2];
+    
+    private static readonly List<Employee> TeamLeads3 =
+    [
+        new(1, "Team-lead 1"),
+        new(2, "Team-lead 2")
+    ];
+    private static readonly List<Employee> Juniors3 =
+    [
+        new(6, "Junior 6"),
+        new(17, "Junior 17")
+    ];
+    private static readonly List<Employee>[] Members3 = [TeamLeads3, Juniors3];
+    
+    private static readonly List<Employee> TeamLeads4 =
+    [
+        new(1, "Team-lead 1"),
+        new(2, "Team-lead 2"),
+        new(3, "Team-lead 3"),
+        new(4, "Team-lead 4")
+    ];
+    private static readonly List<Employee> Juniors4 =
+    [
+        new(101, "Junior 101"),
+        new(102, "Junior 102"),
+        new(103, "Junior 103"),
+        new(104, "Junior 104"),
+        new(105, "Junior 105"),
+        new(106, "Junior 106"),
+        new(107, "Junior 107")
+    ];
+    private static readonly List<Employee>[] Members4 = [TeamLeads4, Juniors4];
+    
+    private static readonly List<List<Employee>[]> Data =
+    [
+        Members1,
+        Members2,
+        Members3,
+        Members4
     ];
 
     public IEnumerator<object[]> GetEnumerator()
     {
-        return _data.GetEnumerator();
+        return Data.GetEnumerator();
     }
 
     IEnumerator IEnumerable.GetEnumerator()
